9df0ada821a4cdeec9796f865157401b863172a6
hello@frances.ink <jeninedrew@gmail.com>
2018-06-07 09:14:24 -0400

    added Menu component tot test router

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "../css/Header.css";
 import logo from "../images/logo.svg";
+import Menu from "../components/Menu";
 
 class Header extends Component {
   render() {
     return (
       <header className="Header">
         <img src={logo} className="Header-logo" alt="logo" />
+        <Menu />
       </header>
     );
   }
